3f7f3c71b3eb07d70f5d47c30265cf2d92441212
Amit kumar <ramitgzp56@gmailcom>
2023-05-02 21:07:56 +0530

    hello

---
+++ script.js
@@ -0,0 +1,44 @@
+//  if(window.screen.width<=760px)
+function openNav() {
+  
+  document.getElementById("main-navigation").style.display="block";
+  document.getElementsByClassName("closebtn").style.display="block";
+
+}
+
+function closeNav() {
+  
+  document.getElementById("main-navigation").style.display="none";
+  document.getElementsByClassName("closebtn").style.display="none";
+
+}
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 5,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      320:{
+        slidesPerView: 2,
+        spaceBetween: 7,
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 7,
+      },
+      768: {
+        slidesPerView: 5,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 13,
+      },
+    },
+  });
