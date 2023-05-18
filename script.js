8326e48c15546c8a2bf64c3df8948c337231d967
Amit kumar <ramitgzp56@gmail.com>
2023-05-18 23:17:13 +0530

    responsive navbar

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
 //  if(window.screen.width<=760px)
-function openNav() {
-  
-  document.getElementById("main-navigation").style.display="block";
-  document.getElementsByClassName("closebtn").style.display="block";
 
-}
-
-function closeNav() {
-  
-  document.getElementById("main-navigation").style.display="none";
-  document.getElementsByClassName("closebtn").style.display="none";
-
-}
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 7,
-    // loop:false,
+function openclose() {
+ let navbar=document.querySelector(".navbar");
+ let humburger=document.querySelector(".humburger");
+  if (navbar.style.display !== "block") {
+    navbar.style.display = "block";
+    
+    humburger.style.rotate=90+"deg";
    
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+
+  }else{
+    navbar.style.display = "none";
+   
+    humburger.style.rotate=0+"deg";
+   
+   
+  }
+}
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 7,
+  // loop:false,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 3,
+      spaceBetween: 7,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 9,
     },
-    breakpoints: {
-      320:{
-        slidesPerView: 2,
-        spaceBetween: 7,
-      },
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 9,
-      },
-      768: {
-        slidesPerView: 5,
-        spaceBetween: 12,
-      },
-      1024: {
-        slidesPerView: 6,
-        spaceBetween: 15,
-      },
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 12,
     },
-  });
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 15,
+    },
+  },
+});
